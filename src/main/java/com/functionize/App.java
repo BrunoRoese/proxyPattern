ddd8ff1a945d5a1eb9315a7e709f4f8d3bb226c8
brunoroese <bruno.roese@functionize.com>
2025-03-27 07:55:23 -0300

    initial commit and project setup

---
+++ src/main/java/com/functionize/App.java
@@ -0,0 +1,13 @@
+package com.functionize;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * Hello world!
+ *
+ */
+@EnableFeignClients
+public class App {
+    public static void main( String[] args ) {
+    }
+}
